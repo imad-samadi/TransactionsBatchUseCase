de0df73d0c52942b6a7f841b74ecfd81e4ab8a78
imad-samadi <imadsamadi999@gmail.com>
2025-05-16 14:22:04 +0000

    first commit

---
+++ src/main/java/com/S2M/TransactionsBatchUseCase/TransactionsBatchUseCaseApplication.java
@@ -0,0 +1,13 @@
+package com.S2M.TransactionsBatchUseCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsBatchUseCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsBatchUseCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/S2M/TransactionsBatchUseCase/TransactionsBatchUseCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S2M.TransactionsBatchUseCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsBatchUseCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
